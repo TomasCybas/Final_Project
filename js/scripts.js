b50da6a846df3852fac1d0f4fcd19249d0a9ba44
Tomas <tcybas@gmail.com>
2019-06-20 21:19:44 +0300

    finished video section of footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 "use strict";
 
 function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+// function to stop video playback on modal close
+$(document).ready(function () {
+    var url = $('#custom_video').attr('src');
+    console.log(url);
+    $('#video_modal').on('hide.bs.modal', function () {
+        $('#custom_video').attr('src', '')
+    })
+        .on('show.bs.modal', function () {
+            $('#custom_video').attr('src', url)
+        });
+});
